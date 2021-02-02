6e486d280af9c07ea70ae8351ae303df84475491
wiggypig <74166360+wiggypig@users.noreply.github.com>
2021-02-02 10:22:29 -0600

    initial commit via upload
    Address book project

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Add first entry to AddressBook directly - implementation is static
+        AddressBook.addEntry(
+                new Entry("Scott",
+                        "Horne",
+                        "5555555",
+                        "develops@scottiehorne.com"));
+    }
+
+}
